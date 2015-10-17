15e2372f165553e0f08d72a9479b640f73a7d217
youaregreathk <youaregreathk@gmail.com>
2015-10-17 11:00:01 -0700

    Bit Manipulation

--- Is-Number-Power-of-Two.cpp
+++ Is-Number-Power-of-Two.cpp
@@ -1,12 +1,13 @@
 /**************************************************************************
 Write 2 functions to determine if a given function is a power of 2 or not
+Given an integer, write a function to determine if it is a power of two.
 
 1. Recursive
    Continue to % by2 to see if the result is!=0
 
 2. Straight forward
    
-
+https://leetcode.com/problems/power-of-two/
 
 ***********************************************************************/
 #include <iostream>
@@ -15,6 +16,8 @@ using namespace std;
 
 //Recursive
 bool IsPowerof2_Recursive(int num){
+  if(num==0)
+    return false;
   if(num==1){
     //cout<<"YO";
     return true;
